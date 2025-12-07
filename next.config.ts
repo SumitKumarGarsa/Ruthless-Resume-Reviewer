c667209cb61d323df534a5d52d60c5a32e79a03f
Sumit Kumar Garsa <sumitkumargarsa@gmail.com>
2025-12-07 19:23:27 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // @ts-ignore
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
